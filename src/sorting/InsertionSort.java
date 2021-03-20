c67e9f837aa882577296a3fb52c379f13a039d5c
Vaibhav Udaywal <vaibhav.udaywal@otssolutions.com>
2021-03-20 11:15:40 +0530

    insertion sort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void sort(int[] arr) {
+
+        int temp = arr[0];
+        for (int i=1; i<arr.length; i++) {
+            for (int j=i; j>0; j--) {
+                // System.out.println("i= "+i+" j= "+j);
+                if (arr[j-1] > arr[j]) {
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        print(arr);
+
+    }
+
+    static void print(int[] arr) {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {56,75,11,7,99,1,4,12};
+        sort(arr);
+    }
+    
+}
--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,10 +1,8 @@
-public class SelectionSort
-{
-    public static void sort(int arr[])
-    {
+public class SelectionSort {
+
+    public static void sort(int arr[]) {
         int n = arr.length;
-        for (int i = 0; i < n-1; i++)
-        {
+        for (int i = 0; i < n-1; i++) {
             int min = i;
             for (int j = i+1; j < n; j++)
                 if (arr[j] < arr[min])
@@ -17,16 +15,14 @@ public class SelectionSort
         }
     }
 
-    static void print(int arr[])
-    {
+    static void print(int arr[]) {
         int n = arr.length;
         for (int i=0; i<n; ++i)
             System.out.print(arr[i]+" ");
         System.out.println();
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         int arr[] = {56,75,11,7,99};
         sort(arr);
         print(arr);
